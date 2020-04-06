2aa1997c3021a19392c9af9a587db8f66d45df75
shchuka-dmitriy <dmitriy.shchuka.rule@gmail.com>
2020-04-06 12:38:30 +0300

    Refactor server structure:    - changed index.js    - express and http settings from index.js moved to folder config, to expressConfig.js and httpServerConfig.js

--- server/src/config/httpServer/httpServerConfig.js
+++
@@ -1,8 +0,0 @@
-import configExpress from "./configureExpress";
-const http = require('http');
-const PORT = process.env.PORT || 9632;
-
-const httpServer = http.createServer(configExpress);
-httpServer.listen(PORT);
-
-export default httpServer;
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,28 @@
-require('./dbMongo/mongoose');
-import httpServer from "./boot/configureHTTPServer";
+/*Подключаем и express и socketIo одновременно*/
 
+require('./dbMongo/mongoose');                                                 /*импортируем конфиги mongoose*/
+const http = require('http');                                                         /*подключаем модуль http, нодовский дефольный*/
+const express = require('express');
+const router = require('./router');
+const cors = require('cors');                                                         /*подключаем middleware для cors origin resorses*/
 const controller = require('./socketInit');
+const handlerError = require('./handlerError/handler');
 
-controller.createConnection(httpServer);
+
+const PORT = process.env.PORT || 9632;                                                /*пдключаем порт, если в переменном окружении не указан то берем 9632*/
+const app = express();                                                                /*создается экземпляр express*/
+
+    /*монтируются middlewares*/
+app.use(cors());
+app.use(express.json());                                        /*middleware чтобы парсить json автоматически, десерриализовать*/
+app.use('/public', express.static('public'));                     /*прописываем где будут находится статические файлы*/
+app.use(router);                                                      /*монтируем маршрутизатор*/
+app.use(handlerError);                                                /*монтируем обработчик который должен идти вконце*/
+
+    /*В сервер засовывается app, а сервер засовывается в socketIo (controller)*/
+const server = http.createServer(app);                                /*создаем сервер http передаем туда экспресс приложение, будет как обертка приложению*/
+server.listen(PORT/*,
+  () => console.log(`Example app listening on port ${ PORT }!`)*/);
+controller.createConnection(server);                                  /*контроллеру socket передаем сервер*/
+
+
